ca463f8fce99c75c18b9a43d92bf3b84bd012a45
wikitopian <matt@swarmstrategies.com>
2013-06-18 21:01:38 +0000

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+Rainmaker
+=========
+
+Constituent Relationship Management for BuddyPress
+
+Installation
+------------
+
+    The usual.
+
+    BuddyPress must be installed.
+
+Usage
+-----
---
+++ rainmaker.php
@@ -0,0 +1,15 @@
+<?php
+/*
+* Plugin Name: Rainmaker
+* Plugin URI: http://www.github.com/wikitopian/rainmaker
+* Description: Constituent Relationship Manager for BuddyPress
+* Version: 0.1
+* Author: Matt Parrott
+* Author URI: http://www.swarmstrategies.com/matt
+* License: GPLv2
+* */
+
+class Rainmaker {
+}
+
+$rainmaker = new Rainmaker();
